db00219ed0fbd4ab6e27c3b1cb2ec7649593aec5
Werberty <werberty.alexandre@gmail.com>
2022-12-08 15:59:33 -0300

    :test_tube: TEST: resolvendo erro de unique constraint

--- studies/tests/test_studies_home_view.py
+++ studies/tests/test_studies_home_view.py
@@ -6,25 +6,28 @@ from .test_studies_base import StudiesBaseTest
 
 
 class StudiesHomeViewsTest(StudiesBaseTest):
-    def setUp(self) -> None:
-        self.make_user()
-        self.login_user()
-        return super().setUp()
 
     def test_studies_home_view_function_is_correct(self):
         view = resolve(reverse('studies:home'))
         self.assertIs(view.func, home)
 
     def test_studies_home_view_returns_status_code_200(self):
+        self.make_user()
+        self.login_user()
+
         response = self.client.get(reverse('studies:home'))
         self.assertEqual(response.status_code, 200)
 
     def test_studies_home_view_loads_correct_template(self):
+        self.make_user()
+        self.login_user()
+
         response = self.client.get(reverse('studies:home'))
         self.assertTemplateUsed(response, 'studies/pages/studies.html')
 
     def test_studies_home_templates_loads_studies(self):
         self.make_study(subject={'name': 'Matemática'})
+        self.login_user()
 
         response = self.client.get(reverse('studies:home'))
         content = response.content.decode('utf-8')
@@ -34,6 +37,8 @@ class StudiesHomeViewsTest(StudiesBaseTest):
         self.assertEqual(len(response_context_studies), 1)
 
     def test_studies_home_template_dont_loads_subjects_not_exist(self):
+        self.make_user()
+        self.login_user()
 
         response = self.client.get(reverse('studies:home'))
 
